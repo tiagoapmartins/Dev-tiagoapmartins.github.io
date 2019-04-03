8f6db3ea6092f2139ebac90d10af2836ef7c8514
Tiago Martins <tiago.a.p.martins@protonmail.ch>
2019-04-03 17:50:22 +0100

    New changes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <SideBar lang="jp" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import SideBar from "./components/SideBar.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    SideBar
   }
 };
 </script>
 
 <style lang="scss">
+html,
+body {
+    margin: 0;
+    padding: 0;
+    height: 100%; /* needed for container min-height */
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 00px;
+  height: 100%;
 }
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="sideBar">
+        <ul class="langSelect">
+            <li>
+                <a href="#">English</a>
+            </li>
+            <li>
+                <a href="#">Português</a>
+            </li>
+            <li>
+                <a href="#">Español</a>
+            </li>
+            <li>
+                <a href="#">日本語</a>
+            </li>
+        </ul>
+        <div>
+            <img src="../assets/me.jpg">
+        </div>
+        <div>
+            <h2 class="name">Tiago Martins</h2>
+            <h4 v-if="lang=='jp'" class="pronunciation">ティアゴ　マルチンス</h4>
+            <p class="shortBio">
+                {{ bio }}
+            </p>
+        </div>
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Academics</a>
+                    </li>
+                    <li>
+                        <a href="#">Projects</a>
+                    </li>
+                     <li>
+                        <a href="#">Contacts</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  props: {
+    lang: String, 
+    bio: String
+  },
+  data: {
+    en_bio: 'Default bio in English',
+    pt_bio: 'Biografia Portuguesa',
+    es_bio: 'Biografia en Español',
+    jp_bio: '日本語のビオぐらふぃ'
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.sideBar {
+    float: left;
+    height: 100%;
+    background: #151515;
+}
+img {
+     border-radius: 50%;
+     max-width: 300px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>      
